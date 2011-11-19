94be2aec0a5e64c70daa67d7387b4270af7e6638
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-11-19 11:06:13 +0900

    FromとToヘッダについてもiso-2022-jpに変換

--- lib/mail_ja.rb
+++ lib/mail_ja.rb
@@ -9,17 +9,13 @@ module Mail
     alias_method :process_body_raw_without_iso_2022_jp_encoding, :process_body_raw
     alias_method :process_body_raw, :process_body_raw_with_iso_2022_jp_encoding
   end
-
-  class SubjectField < UnstructuredField
-    def initialize_with_iso_2022_jp_encoding(value = nil, charset = 'utf-8')
-      if charset.to_s.downcase == 'iso-2022-jp'
-        value = NKF.nkf('--cp932 -M', NKF.nkf('--cp932 -j', value)).gsub("\n", '').strip
-      end
-      initialize_without_iso_2022_jp_encoding(value, charset)
+  
+  module Iso2022JpDecoder
+    def self.included(base)
+      base.send :alias_method, :do_decode_without_iso_2022_jp_encoding, :do_decode
+      base.send :alias_method, :do_decode, :do_decode_with_iso_2022_jp_encoding
     end
-    alias_method :initialize_without_iso_2022_jp_encoding, :initialize
-    alias_method :initialize, :initialize_with_iso_2022_jp_encoding
-
+    
     private
     def do_decode_with_iso_2022_jp_encoding
       if charset.to_s.downcase == 'iso-2022-jp'
@@ -28,7 +24,44 @@ module Mail
         do_decode_without_iso_2022_jp_encoding
       end
     end
-    alias_method :do_decode_without_iso_2022_jp_encoding, :do_decode
-    alias_method :do_decode, :do_decode_with_iso_2022_jp_encoding
+  end
+
+  class SubjectField < UnstructuredField
+    include Iso2022JpDecoder
+    
+    def initialize_with_iso_2022_jp_encoding(value = nil, charset = 'utf-8')
+      if charset.to_s.downcase == 'iso-2022-jp'
+        value = NKF.nkf('--cp932 -M', NKF.nkf('--cp932 -j', value)).gsub("\n", '').strip
+      end
+      initialize_without_iso_2022_jp_encoding(value, charset)
+    end
+    alias_method :initialize_without_iso_2022_jp_encoding, :initialize
+    alias_method :initialize, :initialize_with_iso_2022_jp_encoding
+  end
+
+  class FromField < StructuredField
+    include Iso2022JpDecoder
+    
+    def initialize_with_iso_2022_jp_encoding(value = nil, charset = 'utf-8')
+      if charset.to_s.downcase == 'iso-2022-jp'
+        value = NKF.nkf('--cp932 -M', NKF.nkf('--cp932 -j', value)).gsub("\n", '').strip
+      end
+      initialize_without_iso_2022_jp_encoding(value, charset)
+    end
+    alias_method :initialize_without_iso_2022_jp_encoding, :initialize
+    alias_method :initialize, :initialize_with_iso_2022_jp_encoding
+  end
+
+  class ToField < StructuredField
+    include Iso2022JpDecoder
+    
+    def initialize_with_iso_2022_jp_encoding(value = nil, charset = 'utf-8')
+      if charset.to_s.downcase == 'iso-2022-jp'
+        value = NKF.nkf('--cp932 -M', NKF.nkf('--cp932 -j', value)).gsub("\n", '').strip
+      end
+      initialize_without_iso_2022_jp_encoding(value, charset)
+    end
+    alias_method :initialize_without_iso_2022_jp_encoding, :initialize
+    alias_method :initialize, :initialize_with_iso_2022_jp_encoding
   end
 end
--- test/mail_ja_test.rb
+++ test/mail_ja_test.rb
@@ -8,31 +8,35 @@ class MailJaTest < ActiveSupport::TestCase
   test "ISO-2022-JP" do
     mail = Iso2022jpMailer.notice
     assert_equal NKF::JIS, NKF.guess(mail.subject)
-    assert_equal '=?ISO-2022-JP?B?GyRCRnxLXDhsN29MPhsoQg==?=', mail.subject
+    assert_equal "From: =?ISO-2022-JP?B?GyRCOzNFREJATzobKEI=?= <bar@example.com>\r\n", mail[:from].encoded
+    assert_equal "To: =?ISO-2022-JP?B?GyRCOjRGIzJWO1IbKEI=?= <foo@example.com>\r\n", mail[:to].encoded
+    assert_equal "Subject: =?ISO-2022-JP?B?GyRCRnxLXDhsN29MPhsoQg==?=\r\n", mail[:subject].encoded
     assert_equal NKF::JIS, NKF.guess(mail.body.encoded)
   end
 
   test "ORIGIN" do
     mail = OriginMailer.notice
     assert_equal NKF::UTF8, NKF.guess(mail.subject)
-    assert_equal "\346\227\245\346\234\254\350\252\236\344\273\266\345\220\215", mail.subject
+    assert_equal "From: =?UTF-8?B?5bGx55Sw5aSq6YOO?= <bar@example.com>\r\n", mail[:from].encoded
+    assert_equal "To: =?UTF-8?B?5L2Q6Jek6Iqx5a2Q?= <foo@example.com>\r\n", mail[:to].encoded
+    assert_equal "Subject: =?UTF-8?Q?=E6=97=A5=E6=9C=AC=E8=AA=9E=E4=BB=B6=E5=90=8D?=\r\n", mail[:subject].encoded
     assert_equal NKF::UTF8, NKF.guess(mail.body.encoded)
   end
 end
 
 class Iso2022jpMailer < ActionMailer::Base
-  default :from => "bar@example.com", :charset => 'ISO-2022-JP'
+  default :from => "山田太郎 <bar@example.com>", :charset => 'ISO-2022-JP'
   def notice
-    mail(:to => 'foo@example.com', :subject => '日本語件名') do |format|
+    mail(:to => '佐藤花子 <foo@example.com>', :subject => '日本語件名') do |format|
       format.text { render :inline => '日本語本文' }
     end
   end
 end
 
 class OriginMailer < ActionMailer::Base
-  default :from => "bar@example.com"
+  default :from => "山田太郎 <bar@example.com>"
   def notice
-    mail(:to => 'foo@example.com', :subject => '日本語件名') do |format|
+    mail(:to => '佐藤花子 <foo@example.com>', :subject => '日本語件名') do |format|
       format.text { render :inline => '日本語本文' }
     end
   end
