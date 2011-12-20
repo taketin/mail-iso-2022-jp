ae776dc1b18024fb92c3fe64fc7fa6e4fa8061e4
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-20 22:45:44 +0900

    複数バージョンのRuby/Railsでテスト

--- Gemfile
+++ Gemfile
@@ -2,8 +2,17 @@ source :rubygems
 
 gem "mail", ">= 2.2.5"
 
+rails_version = ENV['MAIL_ISO_2022_JP_RAILS_VERSION']
+
 group :development, :test do
   gem "rake"
   gem "bundler"
-  gem "actionmailer", ">= 3.0.0"
+  if rails_version == "edge"
+    gem "actionmailer", :git => "git://github.com/rails/rails.git"
+  elsif rails_version && rails_version.strip != ""
+    gem "actionmailer", rails_version
+  else
+    gem "actionmailer", ">= 3.0.0"
+  end
+  gem "rdoc", ">= 3.12"
 end
---
+++ test_on_multiple_environments.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
+
+function run {
+  gem list --local bundler | grep bundler || gem install bundler --no-ri --no-rdoc
+
+  for version in 3.0.11 3.1.3 3.2.0.rc1
+  do
+    echo "Running bundle exec rspec spec against rails $version..."
+    MAIL_ISO_2022_JP_RAILS_VERSION=$version bundle update mail rails
+    MAIL_ISO_2022_JP_RAILS_VERSION=$version bundle exec rake test
+  done
+}
+
+rvm use ruby-1.8.7@mail-iso-2022-jp --create
+run
+
+rvm use ruby-1.9.3@mail-iso-2022-jp --create
+run
+
+echo 'Success!'
