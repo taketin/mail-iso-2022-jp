dd03425971151dd48eeb37d945baafec1e636f63
taketin <tksthdnr@gmail.com>
2013-04-04 12:14:23 +0900

    受信メールオブジェクト作成時、２重にエンコードされない様にした

--- lib/mail-iso-2022-jp/field.rb
+++ lib/mail-iso-2022-jp/field.rb
@@ -11,9 +11,15 @@ module Mail
             "The '#{name}' field is not encoded in UTF-8 nor in US-ASCII but in #{value.encoding}")
         end
       end
+      charset = 'utf-8' if already_encoded?(name, charset)
       initialize_without_iso_2022_jp_encoding(name, value, charset)
     end
     alias_method :initialize_without_iso_2022_jp_encoding, :initialize
     alias_method :initialize, :initialize_with_iso_2022_jp_encoding
+
+    private
+    def already_encoded?(name, charset)
+      true if charset == 'ISO-2022-JP' && name =~ /(^Subject:\ \=\?ISO-2022-JP\?)/
+    end
   end
 end
